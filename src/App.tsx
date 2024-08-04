d1e13f5631c0bdc5a21e54602da00d49375eaf21
Zila Oliveira <mottazila@gmail.com>
2024-08-04 11:57:47 -0300

    feat: adding image description

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
   Flex,
-  Input,
+  Center,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 import Header from "./components/Header";
 import Post from "./components/Post";
 import InputContent from "./components/InputContent";
 
-const TOKEN = process.env.REACT_APP_JIGSAWSTACK_API_TOKEN;
+// const TOKEN = process.env.REACT_APP_JIGSAWSTACK_API_TOKEN;
 
 export const App = () => {
 
@@ -28,25 +20,31 @@ export const App = () => {
           flexDir="column"
           textAlign="center" 
           justifyContent="center" 
-          fontSize="xl" w="100%"
+          alignItems="center"
+          fontSize="xl" 
+          w="100%"
           padding={8}
           >
-            <Header />
-            <Box mt={4}>
-              <InputContent />
-            </Box>
-            <Box mt={12} gap={4}>
-              <Post 
-                date="13 June of 2024"
-                content="Rebeca Andrade arrasou nas Olimpíadas desse ano! 🥇🔥 Ela conquistou o ouro e fez história na ginástica artística. Essa mulher é um fenômeno! 🌟 Orgulho do Brasil! 🇧🇷"
-                imageSrc="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/07/26/gn-s12ruam2106q.JPG"
-              />
-              <Post 
-                date="13 June of 2024"
-                content="Rebeca Andrade arrasou nas Olimpíadas desse ano! 🥇🔥 Ela conquistou o ouro e fez história na ginástica artística. Essa mulher é um fenômeno! 🌟 Orgulho do Brasil! 🇧🇷"
-                imageSrc="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/07/26/gn-s12ruam2106q.JPG"
-              />
-            </Box>
+            <Center display="flex"flexDir="column">
+              <Header />
+              <Box maxW="md">
+                <Box mt={4}>
+                  <InputContent />
+                </Box>
+                <Box mt={12} gap={4}>
+                  <Post 
+                    date="13 June of 2024"
+                    content="Rebeca Andrade arrasou nas Olimpíadas desse ano! 🥇🔥 Ela conquistou o ouro e fez história na ginástica artística. Essa mulher é um fenômeno! 🌟 Orgulho do Brasil! 🇧🇷"
+                    imageSrc="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/07/26/gn-s12ruam2106q.JPG"
+                  />
+                  <Post 
+                    date="13 June of 2024"
+                    content="Rebeca Andrade arrasou nas Olimpíadas desse ano! 🥇🔥 Ela conquistou o ouro e fez história na ginástica artística. Essa mulher é um fenômeno! 🌟 Orgulho do Brasil! 🇧🇷"
+                    imageSrc="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/07/26/gn-s12ruam2106q.JPG"
+                  />
+                </Box>
+              </Box>
+            </Center>
         </Flex>
       </ChakraProvider>
     )
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,8 +5,12 @@ import {
   Flex,
   IconButton,
   Stack,
+  Button,
 } from '@chakra-ui/react';
-import { FaShare, FaComment, FaHeart } from 'react-icons/fa';
+import { FaShare, FaComment, FaHeart, FaRegFileAudio, FaSpinner } from 'react-icons/fa';
+import api from '../service/api';
+import { useState } from 'react';
+import axios from 'axios';
 
 interface PostProps {
   date: string;
@@ -15,6 +19,65 @@ interface PostProps {
 }
 
 export default function Post ({ date, content, imageSrc }: PostProps) {
+  const [loading, setLoading] = useState(false);
+  const [imageDesc, setImageDesc] = useState("");
+  const [show, setShow] = useState(false);
+
+  const IMG_API_TOKEN = process.env.REACT_APP_JIGSAWSTACK_IMG_API_TOKEN;
+  const PROMPT_API_TOKEN = process.env.REACT_APP_JIGSAWSTACK_PROMPT_API_TOKEN;
+
+  const handleDescriptionImage = async () => {
+    try {
+      setLoading(true);
+      const response = await api.post("object_detection", {
+        url: imageSrc
+      }, {
+        headers: {
+          "x-api-key": IMG_API_TOKEN
+        },
+      });
+      const words = response?.data?.tags.join(",");
+
+      const newPromptResponse = await axios.post("https://api.jigsawstack.com/v1/prompt_engine", {
+        "prompt": "Hello prompt, can you do a describe of an image with this words? {words}", 
+        "inputs": [
+          {
+            "key": "words",
+            "optional": false,
+            "initial_value": words
+          }
+        ], 
+        "return_prompt": "Return the result in a string format"
+      }, {
+        headers: {
+          "x-api-key": PROMPT_API_TOKEN
+        },
+      });
+
+      const { data } = newPromptResponse;
+      const { prompt_engine_id } = data;
+
+      const promptResponse = await axios.post(`https://api.jigsawstack.com/v1/prompt_engine/${prompt_engine_id}`, {
+        "input_values": {
+          "words": words
+        }
+      }, {
+        headers: {
+          "x-api-key": PROMPT_API_TOKEN
+        },
+      });
+
+      const imageDescription = promptResponse.data.result;
+      setImageDesc(imageDescription);
+      setShow(true);
+
+    } catch(e) {
+      console.log("error", e)
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <Box
       borderWidth="1px"
@@ -26,15 +89,41 @@ export default function Post ({ date, content, imageSrc }: PostProps) {
       mb={4}
     >
       {imageSrc && (
-        <Image
-          src={imageSrc}
-          alt="Post Image"
-          borderRadius="lg"
-          mb={4}
-          objectFit="cover"
-          maxH="200px"
-          w="full"
-        />
+        <>
+          <Flex flexDir="column" alignItems="flex-end">
+            <Image
+              src={imageSrc}
+              alt="Post Image"
+              borderRadius="lg"
+              mb={4}
+              objectFit="cover"
+              maxH="200px"
+              w="full"
+            />
+            <IconButton
+              onClick={handleDescriptionImage}
+              aria-label="Like"
+              isLoading={loading}
+              icon={
+                loading ?  <FaSpinner fontSize={14} color='black' />  : <FaRegFileAudio fontSize={14} color='black' />
+              }
+              variant="ghost"
+              backgroundColor="gray.300"
+              mt={-14}
+              borderRadius={40}
+            />
+          </Flex>
+          {imageDesc.length > 0 && (
+            <Box mt={2} mb={2} display="flex" flexDir="column" alignItems="flex-end">
+              <Button w={14} h={8} size="small" mb={2} onClick={() => setShow(!show)}>
+                <Text fontSize={12}>
+                  {show ? "Hide" : "Show"}
+                </Text>
+              </Button>
+              <Text display={show ? "flex" : "none"} textAlign="left" fontSize={12}>Image description: {imageDesc}</Text>
+            </Box>
+          )}
+        </>
       )}
 
       <Text mb={4} fontSize={16} color="gray.600" textAlign="left">{content}</Text>
